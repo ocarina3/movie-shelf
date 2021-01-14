6fb3cc6b12467e088cfbc8da38c86db2486a22be
Arthur Scanoni <61999297+Arthursca@users.noreply.github.com>
2021-01-14 13:31:00 -0300

    feat: add method to create a user in the user table

--- src/data/base/CreateDatabase.java
+++ src/data/base/CreateDatabase.java
@@ -1,22 +1,26 @@
 package data.base;
 
+import model.entity.User;
+import view.Main;
+
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class CreateDatabase {
 
-    private final Connect c;
-
-    public CreateDatabase(Connect c) {
-        this.c = c;
-    }
+    private final Connect c = new Connect();
 
     // Criação da tabela passando a query, cria a tabela apenas se ela não existe
     public void createTableUser() {
         String query = "CREATE TABLE IF NOT EXISTS user(" +
                 "id integer primary key," +
                 "name varchar," +
-                "email varchar," +
+                "email varchar UNIQUE ON CONFLICT ABORT," +
                 "password varchar," +
                 "birthDate date," +
                 "admin bool" +
@@ -58,6 +62,7 @@ public class CreateDatabase {
         }
     }
 
+
     public void createTableRating() {
         String query = "CREATE TABLE IF NOT EXISTS rating(" +
                 "id integer primary key," +
@@ -78,5 +83,73 @@ public class CreateDatabase {
             if (connected) c.disconnect();
         }
     }
+
+
+
+    //Criação de Usuario passando suas informações e o tipo de usuario
+    private void createUser(User user, boolean admin)
+    {
+        RemoveDatabase r = new RemoveDatabase();
+
+        String sql = "INSERT INTO user(id,name,email,password,birthDate,admin) VALUES(?,?,?,?,?,?);";
+
+        c.connect();
+
+        PreparedStatement p = c.createPreparedStatement(sql);
+
+        try
+        {
+            p.setString(2,user.getName());
+            p.setString(3, user.getEmail());
+            p.setString(4, user.getPassword());
+            p.setDate(5,null);
+            p.setBoolean(6,admin);
+            int teste = p.executeUpdate();
+        }catch (SQLException e)
+        {
+            try {
+                p.setString(2, user.getName());
+                p.setString(3,"NULL");
+                p.setString(4, user.getPassword());
+                p.setDate(5,null);
+                p.setBoolean(6,admin);
+
+
+                int teste = p.executeUpdate();
+                r.deleteUserByEmail("NULL");
+
+                System.out.println("email invalido");
+            }catch (SQLException ex){
+
+                System.out.println("ERRo misterioso");
+            }
+
+            System.out.println("Deu Erro");
+
+        }
+        finally {
+            if(p != null)
+            {
+                try{
+                    p.close();
+                }catch (SQLException ex){
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,ex);
+                    System.out.println("ERROOOO");
+                }
+            }
+            c.disconnect();
+        }
+    }
+
+    public void createClient(User user) {
+        createUser(user,false);
+    }
+
+    public void createAdmin(User user) {
+        createUser(user,true);
+    }
+
+
+
 
 }
