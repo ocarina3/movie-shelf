9031e8b35bfb1f5a4b72448dd6c056745e55d704
Mario Leandro <marioleandro34@gmail.com>
2021-01-27 11:45:38 -0300

    feat: add register controller

--- src/view/register/RegisterController.java
+++ src/view/register/RegisterController.java
@@ -1,14 +1,46 @@
 package view.register;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import view.principal.Main;
 
 public class RegisterController {
 
     @FXML
-    private JFXButton btnVoltar;
+    private JFXButton btnCadastrar;
+
+    @FXML
+    private Label lbCrieSuaConta;
+
+    @FXML
+    private JFXCheckBox cbxKids;
+
+    @FXML
+    private JFXTextField txtfUsername;
+
+    @FXML
+    private JFXTextField txtfTelefone;
+
+    @FXML
+    private JFXTextField txtfEmail;
+
+    @FXML
+    private JFXPasswordField passfSenha;
+
+    @FXML
+    private JFXPasswordField passfConfirmarSenha;
+
+    @FXML
+    private ImageView imgRegisterMan;
+
+    @FXML
+    private Label lbLogo;
 
     @FXML
     void onClick(ActionEvent event) {
