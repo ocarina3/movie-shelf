d3232f8bec8ed07a84689a6f7d21724892d21b82
Mario Leandro <marioleandro34@gmail.com>
2021-01-27 08:40:17 -0300

    feat: add main controller

--- src/view/principal/MainController.java
+++ src/view/principal/MainController.java
@@ -1,4 +1,38 @@
 package view.principal;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Line;
+
 public class MainController {
+
+    @FXML
+    private Label lbLogo;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Label lbWelcome;
+
+    @FXML
+    private Label lbApresentacao;
+
+    @FXML
+    private Label lbServicos;
+
+    @FXML
+    private Line lineWelcome;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    void onClick(ActionEvent event) {
+        Main.changeScreen("register");
+    }
+
+
 }
