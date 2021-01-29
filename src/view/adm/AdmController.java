ccb502254dfa2a5d37b0e60b243b3855870a142c
Mario Leandro <marioleandro34@gmail.com>
2021-01-29 19:24:43 -0300

    feat: add load img file

--- src/view/adm/AdmController.java
+++ src/view/adm/AdmController.java
@@ -1,13 +1,24 @@
 package view.adm;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import view.principal.Main;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdmController {
 
@@ -68,6 +79,35 @@ public class AdmController {
     @FXML
     void backLogin(ActionEvent event) {
         Main.changeScreen("login");
+    }
+
+    @FXML
+    void loadImg(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterJPG =
+                new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+        FileChooser.ExtensionFilter extFilterjpg =
+                new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+        FileChooser.ExtensionFilter extFilterPNG =
+                new FileChooser.ExtensionFilter("PNG files (*.PNG)", "*.PNG");
+        FileChooser.ExtensionFilter extFilterpng =
+                new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+        fileChooser.getExtensionFilters()
+                .addAll(extFilterJPG, extFilterjpg, extFilterPNG, extFilterpng);
+
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            ImageIcon myImageView;
+            imgLock.setImage(image);
+        } catch (IOException ex) {
+            Logger.getLogger(AdmController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
