d7e726c1e072c14c3b3c9005462001fe041ee3cb
LuanAccioly <acciolyluan@gmail.com>
2021-01-30 18:54:22 -0300

    add: atributes to ChangeData scene controller

--- src/view/changeData/ChangeDataController.java
+++ src/view/changeData/ChangeDataController.java
@@ -1,5 +1,8 @@
 package view.changeData;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
 import javax.swing.text.html.ImageView;
@@ -12,6 +15,26 @@ public class ChangeDataController {
 
     @FXML
     private Rectangle rctBackground;
+
+    @FXML
+    private JFXTextField txtNomeUsuario;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXDatePicker txtData;
+
+    @FXML
+    private JFXTextField txtSenha;
+
+    @FXML
+    private JFXTextField txtConfirmarSenha;
+
+    @FXML
+    private JFXButton btnAlterar;
+
+
 
 
 }
