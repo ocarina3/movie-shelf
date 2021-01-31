a470a6c708b257ed0eeaefa1ddca608f0f59a02a
LuanAccioly <acciolyluan@gmail.com>
2021-01-31 01:56:49 -0300

    Revert "Revert "feat: add change user data screen""
    This reverts commit f2e4e88e491726c413e35cf2e670a66c81fc6ddc.

--- src/view/changeData/ChangeDataController.java
+++ src/view/changeData/ChangeDataController.java
@@ -2,13 +2,22 @@ package view.changeData;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import model.ModelUser;
+import model.entity.User;
+import utils.Dialog;
+import view.principal.Main;
 
 import javax.swing.text.html.ImageView;
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ChangeDataController {
+public class ChangeDataController implements Initializable {
 
     @FXML
     private ImageView imgAlterarDados;
@@ -17,23 +26,43 @@ public class ChangeDataController {
     private Rectangle rctBackground;
 
     @FXML
-    private JFXTextField txtNomeUsuario;
+    private JFXTextField txtfUser;
 
     @FXML
-    private JFXTextField txtEmail;
+    private JFXTextField txtfEmail;
 
     @FXML
-    private JFXDatePicker txtData;
+    private JFXDatePicker dtBirthdate;
 
     @FXML
-    private JFXTextField txtSenha;
+    private JFXPasswordField pfPass;
 
     @FXML
-    private JFXTextField txtConfirmarSenha;
+    private JFXPasswordField pfConfirmPass;
 
     @FXML
     private JFXButton btnAlterar;
 
+    @FXML
+    private JFXButton btnQuit;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Main.addOnChangesScreenListener(new Main.OnChangeScreen() {
+            @Override
+            public void onScreenChanged(String newScreen, String currentUser) {
+                    txtfUser.setText(ModelUser.getInstance().readUsersByEmail(currentUser).getName());
+                    txtfEmail.setText(ModelUser.getInstance().readUsersByEmail(currentUser).getEmail());
+                    dtBirthdate.setValue(ModelUser.getInstance().readUsersByEmail(currentUser).getBirthDate());
+            }
+        });
+    }
+
+    public void changeRegister(ActionEvent event) {
+    }
+
+    public void backHome(ActionEvent event) {Main.changeScreen("home");}
 
 
 
