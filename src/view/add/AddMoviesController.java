cbc6a3c9060deac3ddbb364e2de3dfc6fdbb6078
Mario Leandro <marioleandro34@gmail.com>
2021-02-01 11:48:54 -0300

    fix: update search screen label

--- src/view/add/AddMoviesController.java
+++ src/view/add/AddMoviesController.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
 import model.ModelMovie;
 import model.ModelUser;
 import model.entity.Genre;
@@ -114,7 +115,5 @@ public class AddMoviesController implements Initializable {
                 }
             }
         });
-
-
     }
 }
