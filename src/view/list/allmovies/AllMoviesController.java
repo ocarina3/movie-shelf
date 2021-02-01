c63efed59e44bd62d5faabbee403c146c3357d90
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-02-01 11:34:31 -0300

    refactor: move all movies classes to all movies package

--- src/view/list/allmovies/AllMoviesController.java
+++ src/view/list/allmovies/AllMoviesController.java
@@ -1,4 +1,4 @@
-package view.list;
+package view.list.allmovies;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 import model.ModelMovie;
 import model.ModelRating;
 import model.entity.Movie;
+import view.list.InfoMoviesController;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -44,7 +45,7 @@ public class AllMoviesController implements Initializable {
                 {
                     InfoMoviesController.movieId = movie.getId();
                     try {
-                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("infomovies.fxml"));
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../infomovies.fxml"));
                         Parent root1 = (Parent) fxmlLoader.load();
                         Stage stage = new Stage();
                         stage.setTitle("Info");
