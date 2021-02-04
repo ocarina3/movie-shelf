1a4e1ba486086a3c4fa7505041b1917243db3dd3
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-02-04 17:08:24 -0300

    fix(utils): remove unnecessary code of the encryption

--- src/utils/EncryptPassword.java
+++ src/utils/EncryptPassword.java
@@ -20,52 +20,6 @@ public class EncryptPassword {
         String senhahexAdmin = hexStringSenhaAdmin.toString();
 
         System.out.println(senhahexAdmin);
-
-
-
-        //-------------- Senha User
-
-        String senhaUser = "user";
-
-        byte messageDigestSenhaUser[] = algorithm.digest(senhaUser.getBytes("UTF-8"));
-
-        StringBuilder hexStringSenhaUser = new StringBuilder();
-        for (byte b : messageDigestSenhaUser) {
-            hexStringSenhaUser.append(String.format("%02X", 0xFF & b));
-        }
-        String senhahexUser = hexStringSenhaUser.toString();
-
-        System.out.println(senhahexUser);
-
-
-
-        //--Comparando Senha User e Admin
-
-        System.out.println(senhahexUser.equals(senhahexAdmin));
-
-
-
-        //-------------- Senha Admin
-
-        String senhaAdminNova = "admin";
-
-        byte messageDigestSenhaAdminNova[] = algorithm.digest(senhaAdminNova.getBytes("UTF-8"));
-
-        StringBuilder hexStringSenhaAdminNova = new StringBuilder();
-        for (byte b : messageDigestSenhaAdminNova) {
-            hexStringSenhaAdminNova.append(String.format("%02X", 0xFF & b));
-        }
-        String senhahexAdminNova = hexStringSenhaAdminNova.toString();
-
-        System.out.println(senhahexAdminNova);
-
-
-
-        //--Comparando Senha User e Admin
-
-        System.out.println(senhahexAdminNova.equals(senhahexAdmin));
-
-
     }
 
 }
