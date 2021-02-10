80cdf2c6a226f53cfabe9a29fd20d51931cad391
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-02-10 18:45:45 -0300

    feat: add comments in the class ValidateEmail

--- src/utils/ValidateEmail.java
+++ src/utils/ValidateEmail.java
@@ -2,6 +2,9 @@ package utils;
 
 public class ValidateEmail {
 
+    /**
+     * Verifica se o formato do email passado é valido
+     * */
     public static boolean isValidEmail(String email) {
         String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
