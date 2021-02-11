1d9f71efdee7162916e9a3400ed945601cdee28f
Arthur Scanoni <arthur.scanoni@hotmail.com>
2021-02-11 15:10:49 -0300

    feat: add comments in the class ComedyMoviesController

--- src/view/list/comedy/ComedyMoviesController.java
+++ src/view/list/comedy/ComedyMoviesController.java
@@ -38,6 +38,11 @@ public class ComedyMoviesController implements Initializable {
     @FXML
     private Label lbGender;
 
+    /**
+     * Busca quais filmes do banco de dados que tem gênero
+     * de comedia e mostra na tela
+     * */
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         int posX = 74;
